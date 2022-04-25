e97a0673f1cc422e1f91e3d41a717795e7375758
Dylan Watson <lotsabackscatter@gmail.com>
2022-04-25 20:01:20 +0800

    Make ball bouncy

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
 {
+    float speed = 4;
     Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(4, 0, 4), ForceMode.Impulse);
+        rb.AddForce(new Vector3(speed, 0, speed), ForceMode.Impulse);
     }
 
     void Update()
