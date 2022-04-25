703e56b4ae555dfcb6dd3f713de165b245dcdaea
Dylan Watson <lotsabackscatter@gmail.com>
2022-04-25 15:17:29 +0800

    Add basic setup for pong

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left")) {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        } else if(Input.GetKey("right")) {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
