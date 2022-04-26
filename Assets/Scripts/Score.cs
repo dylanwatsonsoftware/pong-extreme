122123fa40c3408daec68fa7e7a62c0463ab316c
Dylan Watson <lotsabackscatter@gmail.com>
2022-04-26 13:01:56 +0800

    Add a score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Text))]
 public class Score : MonoBehaviour
 {
+    public Paddle paddle;
+    Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        text.text = paddle.score.ToString();
     }
 }
