02a45116cdfd59f839a1fc92868a237d7a9348fb
Dylan Watson <lotsabackscatter@gmail.com>
2022-04-26 16:33:29 +0800

    remove comments

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -14,12 +14,10 @@ public class Paddle: MonoBehaviour {
     private Vector2 touchStartPosition, touchEndPosition;
     private string direction; 
 
-  // Start is called before the first frame update
   void Start() {
 
   }
 
-  // Update is called once per frame
   void Update() {
     if (Input.GetKey(left)) {
       transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -9,13 +9,11 @@ public class Score : MonoBehaviour
     public Paddle paddle;
     Text text;
 
-    // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         text.text = paddle.score.ToString();
