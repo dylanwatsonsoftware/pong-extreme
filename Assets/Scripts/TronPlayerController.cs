aecbf56c6d514e99fa9c5c8d47d0d95c1c840a63
Dylan Watson <lotsabackscatter@gmail.com>
2022-06-22 20:51:04 +0800

    Import Input System

--- Assets/Scripts/TronPlayerController.cs
+++ Assets/Scripts/TronPlayerController.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using Mirror;
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
-#endif
 
 /* Note: animations are called via the controller for both the character and capsule using animator null checks
  */
